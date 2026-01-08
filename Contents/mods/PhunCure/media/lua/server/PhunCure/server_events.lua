1677fbcc962339441694ef6102dddb969d34cab0
PhunZoider <projectphunzoid@gmail.com>
2026-01-08 19:14:42 +0000

    Feat: Expired Cure Chance

--- Contents/mods/PhunCure/media/lua/server/PhunCure/server_events.lua
+++ Contents/mods/PhunCure/media/lua/server/PhunCure/server_events.lua
@@ -15,8 +15,24 @@ Events.OnZombieDead.Add(function(zed)
 
     local outfit = zed:getOutfitName()
     if tostring(outfit) == "HazardSuit" then
+        local expiredChance = Core.getOption("ExpiredChance", 0)
+        if expiredChance > 0 then
+            local roll = ZombRand(1, 101)
+            if roll <= expiredChance then
+                Core.debugLn("Zombie " .. tostring(Core.getZId(zed)) .. " dropped an expired cure (roll " ..
+                                 tostring(roll) .. " <= " .. tostring(expiredChance) .. ")")
+                local inventory = zed:getInventory()
+                local items = zed:getInventory():AddItems("PhunCure.Cure", 1)
+                for i = 0, items:size() - 1 do
+                    items:get(i):setAge(items:get(i):getOffAgeMax() + ZombRand(1, 10));
+                    -- sendAddItemToContainer(zed:getInventory(), items:get(i));
+                end
+                return
+            end
+        end
         local inventory = zed:getInventory()
         local item = zed:getInventory():AddItems("PhunCure.Cure", 1)
+
         -- sendAddItemsToContainer(zed:getInventory(), item);
     end
 end);
